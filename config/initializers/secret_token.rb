f8d956a2f0aa3e7f364059497bf266dcfb12b4b2
Quentin Calvez <quentin.calvez@quentez.com>
2012-09-07 16:00:38 +0200

    First commit with blank Rails 3 App.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Traduco::Application.config.secret_token = 'fae62423e9e13839731e9e7c95b8c02c09b7d2e97094ebc125308cd9b2591357ec335e9510280e5f375867ff54f9535510a14fbc0625beffae191cff9b173e7f'
