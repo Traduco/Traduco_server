881c08c7a6db3de1481e809a46ef49a3d788c0e6
Pierre Laurac <pierre.laurac@gmail.com>
2012-09-09 15:14:39 +0200

    model over

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,107 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120907143311) do
+ActiveRecord::Schema.define(:version => 20120909113259) do
 
-  create_table "users", :force => true do |t|
+  create_table "keys", :force => true do |t|
+    t.string   "key"
+    t.string   "comment"
+    t.integer  "value_id"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+  end
+
+  create_table "languages", :force => true do |t|
+    t.string   "name"
+    t.string   "format"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "languages_users", :id => false, :force => true do |t|
+    t.integer "language_id"
+    t.integer "user_id"
+  end
+
+  add_index "languages_users", ["language_id", "user_id"], :name => "index_languages_users_on_language_id_and_user_id"
+
+  create_table "projects", :force => true do |t|
+    t.string   "name"
+    t.integer  "language_id"
+    t.integer  "repository_id"
+    t.datetime "created_at",    :null => false
+    t.datetime "updated_at",    :null => false
+  end
+
+  create_table "projects_users", :id => false, :force => true do |t|
+    t.integer "project_id"
+    t.integer "user_id"
+  end
+
+  add_index "projects_users", ["project_id", "user_id"], :name => "index_projects_users_on_project_id_and_user_id"
+
+  create_table "repositories", :force => true do |t|
+    t.string   "sshKey"
+    t.string   "name"
+    t.string   "address"
+    t.integer  "repositoryType_id"
+    t.datetime "created_at",        :null => false
+    t.datetime "updated_at",        :null => false
+  end
+
+  create_table "repository_types", :force => true do |t|
+    t.integer  "type"
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "source_types", :force => true do |t|
+    t.string   "name"
+    t.integer  "type"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "sources", :force => true do |t|
+    t.string   "filePath"
+    t.integer  "project_id"
+    t.integer  "sourceType_id"
+    t.datetime "created_at",    :null => false
+    t.datetime "updated_at",    :null => false
+  end
+
+  create_table "translations", :force => true do |t|
+    t.integer  "project_id"
+    t.integer  "language_id"
+    t.integer  "value_id"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
+
+  create_table "translations_users", :id => false, :force => true do |t|
+    t.integer "translation_id"
+    t.integer "user_id"
+  end
+
+  add_index "translations_users", ["translation_id", "user_id"], :name => "index_translations_users_on_translation_id_and_user_id"
+
+  create_table "users", :force => true do |t|
+    t.string   "firstName"
+    t.string   "lastName"
+    t.string   "password"
+    t.string   "email"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "values", :force => true do |t|
+    t.string   "value"
+    t.boolean  "isTranslated"
+    t.boolean  "isStared"
+    t.integer  "key_id"
+    t.datetime "created_at",   :null => false
+    t.datetime "updated_at",   :null => false
   end
 
 end
