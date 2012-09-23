d844950feceb0f04061ffe7e0e66b14c59ec3843
Pierre Laurac <pierre.laurac@gmail.com>
2012-09-23 20:07:15 +0200

    finding localized strings in resx

--- lib/loc_processors/resx_processor.rb
+++ lib/loc_processors/resx_processor.rb
@@ -1,2 +1,17 @@
 class ResxProcessor
+	def find_files (file_paths, language_format)
+		found_file_paths = []
+		file_paths.each do |file_path|
+			if (file_path =~ /.*\.resx/ && file_path !~ /.*\.[a-zA-z\-]{2,5}\.resx/)
+				found_file_paths << file_path
+			end	
+		end
+		found_file_paths
+	end
+
+	def parse_file (file_path)
+	end
+
+	def write_file (data, original_file_path, language_format)
+	end	
 end
