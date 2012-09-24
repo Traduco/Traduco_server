4c3b694aef6af7751d214a47ee417c6a60556e55
Pierre Laurac <pierre.laurac@gmail.com>
2012-09-24 11:52:08 +0200

    Small commit to fix an typo error. Resx writer almost complete. Just need to replace the values in the file, but otherwise the entire algorithm has been written already

--- lib/loc_processors/resx_processor.rb
+++ lib/loc_processors/resx_processor.rb
@@ -18,8 +18,8 @@ class ResxProcessor
 		to_return = []
 		nodes.each do |node|	
 			to_return << {
-				:value		=> node.find_first("value").content
-				:key 		=> node["name"]
+				:value		=> node.find_first("value").content,
+				:key 		=> node["name"],
 				:comment	=> node.find_first("comment") ? node.find_first("comment").content : ""
 			}
 		end
@@ -27,5 +27,25 @@ class ResxProcessor
 	end
 
 	def write_file (data, original_file_path, language_format)
+		# we need first to create the new file_path with the language format
+		#-------------------------------------------------------------------
+		directory_path = File.dirname(original_file_path)
+		file_name = File.basename(original_file_path, ".resx")
+		file_to_create = File.join(directory_path, "#{file_name}.#{language_format}.resx")
+
+		# we now need to load in memory the original file
+		#------------------------------------------------
+		parser = XML::Parser.file(file_path)
+		document = parser.parse
+
+		# we can now modify the values based on the received data
+		#--------------------------------------------------------
+		data.each do |hash|
+		
+		end
+ 
+		# we now have to save this document to a different file
+		#------------------------------------------------------
+		document.save(file_to_create, :indent => true)
 	end	
 end
